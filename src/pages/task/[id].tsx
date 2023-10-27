aa2c0e0d8414b84a95ea1ad567d44bf48ed27ff4
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-10-27 16:34:58 -0300

    Buscando detalhes da task no db e convertendo data

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,6 +1,14 @@
 import Head from 'next/head';
 import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
+import { db } from '../../services/firebaseConnection';
+import { 
+    doc,
+    collection,
+    query,
+    where,
+    getDoc
+} from 'firebase/firestore';
 
 export default function Task() {
     return(
@@ -19,7 +27,39 @@ export default function Task() {
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const id = params?.id as string;
 
-    console.log(id);
+    const docRef = doc(db, "tasks", id);
+
+    const snapshot = await getDoc(docRef);
+
+    if(snapshot.data() === undefined) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    if(!snapshot.data()?.public) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };   
+    }
+
+    const miliseconds = snapshot.data()?.created?.seconds * 1000;
+
+    const task = {
+        tarefa: snapshot.data()?.tarefa,
+        public: snapshot.data()?.public,
+        created: new Date(miliseconds).toLocaleDateString(),
+        user: snapshot.data()?.user,
+        taskId: id,
+    };
+
+    console.log(task);
     return{
         props: {}
     };
