d5729f09d3513b9491cb682852a1bb15674bc713
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-10-27 16:12:18 -0300

    Carregando id da tarefa do lado do servidor

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from './styles.module.css';
+import { GetServerSideProps } from 'next';
 
 export default function Task() {
     return(
@@ -7,6 +8,19 @@ export default function Task() {
             <Head>
                 <title>Detalhes da tarefa</title>
             </Head>
+
+            <main className={styles.main}>
+                <h1>Tarefa</h1>
+            </main>
         </div>
-    )
+    );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const id = params?.id as string;
+
+    console.log(id);
+    return{
+        props: {}
+    };
+};
