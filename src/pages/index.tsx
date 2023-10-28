1bb29a2bd7d0a48acbcf929649a785d9f43ba09d
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-10-27 22:06:49 -0300

    Exibir quantidade de posts e comments na home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from '../../styles/home.module.css';
 import Image from 'next/image';
-
 import heroImg from '../../public/assets/hero.png';
+import { db } from '../services/firebaseConnection';
+import { collection, getDoc, getDocs } from 'firebase/firestore';
 
-export default function Home() {
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -28,13 +35,30 @@ export default function Home() {
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     </div>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  //Buscar do db os números e mandar pro componente
+  const commmentRef = collection(db, "comments");
+  const postRef = collection(db, "tasks");
+
+  const commentSnapshot = await getDocs(commmentRef);
+  const postSnapshot = await getDocs(postRef);
+  
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0 
+    },
+    revalidate: 60, //seria revalidada a cada 60 segundos 
+  };
+};
