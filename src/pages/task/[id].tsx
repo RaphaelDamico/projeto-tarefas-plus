07b913aef2bbe8fc9f638507cfe544477d5b79bd
RaphaelDamico <raphaelvivarinid@gmail.com>
2023-10-27 21:34:34 -0300

    Implementar funcionalidade do trash button

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
 import { db } from '../../services/firebaseConnection';
-import { doc, collection, query, where, getDoc, addDoc, getDocs } from 'firebase/firestore';
+import { doc, collection, query, where, getDoc, addDoc, getDocs, deleteDoc } from 'firebase/firestore';
 import { Textarea } from '../../components/textarea';
 import { FaTrash } from 'react-icons/fa';
 
@@ -63,6 +63,17 @@ export default function Task({ item, allComments }: TaskProps) {
             console.log(err);
         }
     }
+
+    async function handleDeleteComment(id: string) {
+        try {
+            const docRef = doc(db, "comments", id);
+            await deleteDoc(docRef);
+
+            alert("COMENTÁRIO DELETADO COM SUCESSO");
+        }catch(err) {
+            console.log(err);
+        }
+    }
     return(
         <div className={styles.container}>
             <Head>
@@ -102,7 +113,7 @@ export default function Task({ item, allComments }: TaskProps) {
                         <div className={styles.headComment}>
                             <label className={styles.commentsLabel}>{item.name}</label>
                             {item.user === session?.user?.email && (
-                                <button className={styles.buttonTrash}>
+                                <button className={styles.buttonTrash} onClick={() => handleDeleteComment(item.id)}>
                                     <FaTrash size={18} color= "#EA3140"/>
                                 </button>
                             )}
